360a1300e7548c2694776eb10ac78adb60eac45f
Kainos Innovative Solutions <kainosdevelopmentteam@gmail.com>
2022-11-19 17:37:08 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { NgbNavConfig } from '@ng-bootstrap/ng-bootstrap';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -24,21 +25,46 @@ export class LoginComponent implements OnInit {
     this.loginform = this.frm.group({
 
 
-      Email: ['', [Validators.required]],
+      Email: ['', [Validators.required,  Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
       password: ['', [Validators.required]],
 
     });
 
     this.registerform = this.frm.group({
 
-     Name: ['', [Validators.required]],
-     UserName: ['', [Validators.required]],
-      Email: ['', [Validators.required]],
+     FirstName: ['', [Validators.required]],
+     LastName: ['', [Validators.required]],
+      Email: ['', [Validators.required,  Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      Phone: ['', [Validators.required, Validators.pattern("[0-9 ]{10}")]],
       Password: ['', [Validators.required]],
       ConfirmPassword: ['', [Validators.required]],
 
-    });
+    },
+    {
+      validator: this.ConfirmedValidator('Password', 'ConfirmPassword')
+    }
+
+    );
   }
+
+  ConfirmedValidator(passwordKey: string, passwordConfirmationKey: string) {
+    return (group: FormGroup) => {
+
+      let passwordInput = group.controls[passwordKey],
+          passwordConfirmationInput = group.controls[passwordConfirmationKey];
+
+      if (passwordConfirmationInput.value == '') {
+        return passwordConfirmationInput.setErrors({required: true})
+      }
+      else if (passwordInput.value !== passwordConfirmationInput.value) {
+        return passwordConfirmationInput.setErrors({notEquivalent: true})
+      }
+      else {
+          return passwordConfirmationInput.setErrors(null);
+      }
+    }
+  }
+
   form(){
     (<HTMLInputElement>document.getElementById('pills-home')).style.display = "none";
   }
@@ -54,9 +80,11 @@ export class LoginComponent implements OnInit {
   }
   login(){
     this.submitted = true;
+    console.log(this.loginform.value);
   }
   register(){
     this.submitted1 = true;
+    console.log(this.registerform.value);
   }
 
 }

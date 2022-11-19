32969c0a440e9cc79387c9dc64b784702f8e4edb
Kainos Innovative Solutions <kainosdevelopmentteam@gmail.com>
2022-11-19 21:49:09 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,14 +11,13 @@ import { NgbNavConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class LoginComponent implements OnInit {
   loginform: FormGroup | any;
-  registerform: FormGroup | any;
-  active = 1;
-  submitted = false;
-  submitted1 = false;
 
-  constructor(config: NgbNavConfig, private frm:FormBuilder) {
-    config.destroyOnHide = false;
-		config.roles = false;
+
+  submitted = false;
+
+
+  constructor( private frm:FormBuilder) {
+
   }
 
   ngOnInit(): void {
@@ -30,61 +29,20 @@ export class LoginComponent implements OnInit {
 
     });
 
-    this.registerform = this.frm.group({
 
-     FirstName: ['', [Validators.required]],
-     LastName: ['', [Validators.required]],
-      Email: ['', [Validators.required,  Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      Phone: ['', [Validators.required, Validators.pattern("[0-9 ]{10}")]],
-      Password: ['', [Validators.required]],
-      ConfirmPassword: ['', [Validators.required]],
-
-    },
-    {
-      validator: this.ConfirmedValidator('Password', 'ConfirmPassword')
-    }
-
-    );
   }
 
-  ConfirmedValidator(passwordKey: string, passwordConfirmationKey: string) {
-    return (group: FormGroup) => {
 
-      let passwordInput = group.controls[passwordKey],
-          passwordConfirmationInput = group.controls[passwordConfirmationKey];
 
-      if (passwordConfirmationInput.value == '') {
-        return passwordConfirmationInput.setErrors({required: true})
-      }
-      else if (passwordInput.value !== passwordConfirmationInput.value) {
-        return passwordConfirmationInput.setErrors({notEquivalent: true})
-      }
-      else {
-          return passwordConfirmationInput.setErrors(null);
-      }
-    }
-  }
-
-  form(){
-    (<HTMLInputElement>document.getElementById('pills-home')).style.display = "none";
-  }
-  form1(){
-    (<HTMLInputElement>document.getElementById('pills-home')).style.display = "block";
-  }
+ 
 
   get log() {
     return this.loginform.controls;
   }
-  get regi() {
-    return this.registerform.controls;
-  }
+
   login(){
     this.submitted = true;
     console.log(this.loginform.value);
-  }
-  register(){
-    this.submitted1 = true;
-    console.log(this.registerform.value);
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerform: FormGroup | any;
+  submitted1=false;
+  constructor(private frm:FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerform = this.frm.group({
+
+      FirstName: ['', [Validators.required]],
+      LastName: ['', [Validators.required]],
+       Email: ['', [Validators.required,  Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+       Phone: ['', [Validators.required, Validators.pattern("[0-9 ]{10}")]],
+       Password: ['', [Validators.required]],
+       ConfirmPassword: ['', [Validators.required]],
+
+     },
+     {
+       validator: this.ConfirmedValidator('Password', 'ConfirmPassword')
+     }
+
+     );
   }
+
+  ConfirmedValidator(passwordKey: string, passwordConfirmationKey: string) {
+    return (group: FormGroup) => {
+
+      let passwordInput = group.controls[passwordKey],
+          passwordConfirmationInput = group.controls[passwordConfirmationKey];
+
+      if (passwordConfirmationInput.value == '') {
+        return passwordConfirmationInput.setErrors({required: true})
+      }
+      else if (passwordInput.value !== passwordConfirmationInput.value) {
+        return passwordConfirmationInput.setErrors({notEquivalent: true})
+      }
+      else {
+          return passwordConfirmationInput.setErrors(null);
+      }
+    }
+  }
+
+  get regi() {
+    return this.registerform.controls;
+  }
+
+  register(){
+    this.submitted1 = true;
+    console.log(this.registerform.value);
+  }
+
 
 }
